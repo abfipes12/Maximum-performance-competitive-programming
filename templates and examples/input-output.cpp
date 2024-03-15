bd521fc4cef99b08f3684bfe2ee53f77c2d02201
abfipes12 <abfipes12@gmail.com>
2024-03-15 19:57:06 +0100

    new io functions

--- templates and examples/input-output.cpp
+++ templates and examples/input-output.cpp
@@ -1,9 +1,22 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int get_uint()
+{
+    int x{};
+    auto &gu{getchar_unlocked};
+    for (int c = gu(); c >= '0'; c = gu())
+        x = x * 10 + c - '0';
+
+    return x;
+}
+
 // about ??? faster than cout (link)
 void put_uint(int x)
 {
     static char buf[16];
-    char* it = buf;
+    char *it = buf;
     do
     {
         *it++ = x % 10 | '0';
@@ -13,5 +26,30 @@ void put_uint(int x)
         putchar_unlocked(*--it);
 }
 
-TODO  
-https://github.com/KabukiStarship/KabukiToolkit/wiki/Fastest-Method-to-Print-Integers-and-Floating-point-Numbers
+// TODO https : // github.com/KabukiStarship/KabukiToolkit/wiki/Fastest-Method-to-Print-Integers-and-Floating-point-Numbers
+
+char read_char(){static char b[4194304],*s=b,*e=b;return(s==e&&(s=b)==(e=b+fread_unlocked(b,1,4194304,stdin)))?-1:*s++;}
+char read_char()
+{
+    constexpr int in_size = 4 * 1024 * 1024; // 4 MB
+    static char buf[in_size], *beg = buf, *end = buf;
+
+    if (beg == end)
+    {
+        beg = buf;
+        end = buf + fread_unlocked(buf, sizeof(char), in_size, stdin);
+        if (beg == end)
+            return EOF;
+    }
+    return *beg++;
+}
+
+int get_uint(){int x{};for(char c=read_char();c>='0';c=read_char())x=x*10+c-'0';return x;}
+int get_uint()
+{
+    int x{};
+    for (char c = read_char(); c >= '0'; c = read_char())
+        x = x * 10 + c - '0';
+
+    return x;
+}
